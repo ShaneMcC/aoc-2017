4e1fc725fa087eebab9e64ef2f80106296903848
Shane Mc Cormack <shanemcc@gmail.com>
2017-12-01 22:11:56 +0000

    Add alternative method.

--- 1/run.php
+++ 1/run.php
@@ -8,13 +8,18 @@
 		for ($i = 0; $i < strlen($input); $i++) {
 			$n = (int)$input{$i};
 			$next = (int)$input{($i + ($part2 ? strlen($input) / 2 : 1)) % strlen($input)};
-
 			if ($n == $next) {
 				$total += $n;
 			}
 		}
-
 		return $total;
+	}
+
+	// Alternative way that does the same as above, but looks uglier...
+	function getCaptcha2($input, $part2 = false) {
+		return array_sum(array_filter(str_split($input), function ($n, $i) use ($input, $part2) {
+			return $n == $input{($i + ($part2 ? strlen($input) / 2 : 1)) % strlen($input)};
+		}, ARRAY_FILTER_USE_BOTH));
 	}
 
 	echo 'Part 1: ', getCaptcha($input), "\n";
