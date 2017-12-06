a9c8386b2773c0f7487c181647de34050fb56e24
Shane Mc Cormack <shanemcc@gmail.com>
2017-12-06 08:09:21 +0000

    De-stupid part2.

--- 6/run.php
+++ 6/run.php
@@ -5,25 +5,13 @@
 
 	function reallocateBanks($banks, $part2 = false) {
 		$seen = [];
-		$part2Check = NULL;
-		$part2Count = 0;
 
 		while (true) {
 			$checksum = crc32(json_encode($banks));
 			if (in_array($checksum, $seen)) {
-				if (!$part2) { break; }
-
-				if ($part2Check === NULL) {
-					$part2Check = $checksum;
-				} else {
-					$part2Count++;
-					if ($part2Check == $checksum) {
-						break;
-					}
-				}
-			} else {
-				$seen[] = $checksum;
+				return $part2 ? count($seen) - array_search($checksum, $seen) : count($seen);
 			}
+			$seen[] = $checksum;
 
 			$max = max($banks);
 			$location = array_search($max, $banks);
@@ -32,8 +20,6 @@
 				$banks[($location + $i) % count($banks)]++;
 			}
 		}
-
-		return $part2 ? $part2Count : count($seen);
 	}
 
 	echo 'Part 1: ', reallocateBanks($banks), "\n";
