01ef6a4255259b9e10a8508b692529cbc5500a25
Shane Mc Cormack <shanemcc@gmail.com>
2017-12-15 11:01:23 +0000

    Minor rewrite.

--- 15/run.php
+++ 15/run.php
@@ -4,33 +4,55 @@
 	require_once(dirname(__FILE__) . '/../10/KnotHash.php');
 	$input = getInputLines();
 
+	class AOCGenerator {
+		private $start = 0;
+		private $value = 0;
+		private $factor = 0;
+		private $multiple = 1;
+
+		public function __construct($start) {
+			$this->value = $this->start = $start;
+		}
+		public function reset() {
+			$this->value = $this->start;
+		}
+		public function setFactor($factor) {
+			$this->factor = $factor;
+		}
+		public function setMultiple($multiple) {
+			$this->multiple = $multiple;
+		}
+		public function getNext() {
+			do {
+				$this->value = ($this->value * $this->factor) % 2147483647;
+			} while ($this->value % $this->multiple != 0);
+			return $this->value;
+		}
+	}
+
 	$generator = array();
 	// Get data for each program
 	foreach ($input as $details) {
 		preg_match('#Generator ([AB]) starts with ([0-9]+)#SADi', $details, $m);
 		list($all, $name, $start) = $m;
-		$generator[$name] = ['start' => (int)$start, 'value' => (int)$start];
+		$generator[$name] = new AOCGenerator((int)$start);
 	}
 
-	$generator['A']['factor'] = 16807;
-	$generator['B']['factor'] = 48271;
+	$generator['A']->setFactor(16807);
+	$generator['B']->setFactor(48271);
 
 	function getMatches($generator, $part2 = false) {
-		$divider = 2147483647;
-
-		$generator['A']['multiple'] = ($part2) ? 4 : 1;
-		$generator['B']['multiple'] = ($part2) ? 8 : 1;
+		if ($part2) {
+			$generator['A']->setMultiple(4);
+			$generator['B']->setMultiple(8);
+		}
+		$generator['A']->reset();
+		$generator['B']->reset();
 
 		$matches = 0;
 		for ($i = 0; $i < (isTest() ? 5 : ($part2 ? 5000000 : 40000000)); $i++) {
-			foreach ($generator as $name => &$info) {
-				do {
-					$info['value'] = ($info['value'] * $info['factor']) % $divider;
-				} while ($info['value'] % $info['multiple'] != 0);
-			}
-
-			$a = $generator['A']['value'] & 65535;
-			$b = $generator['B']['value'] & 65535;
+			$a = $generator['A']->getNext() & 65535;
+			$b = $generator['B']->getNext() & 65535;
 			if ($a == $b) { $matches++; }
 			if (isDebug() && $i > 0 && $i % 1000000 == 0) { echo $i, "\n"; }
 		}
@@ -38,7 +60,6 @@
 		return $matches;
 	}
 
-	echo 'This will take a while...', "\n";
 	$part1 = getMatches($generator);
 	echo 'Part 1: ', $part1, "\n";
 	$part2 = getMatches($generator, true);
