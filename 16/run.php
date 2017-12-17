bf7a780de07480507ec072f7bf71d9ad1ff16df8
Shane Mc Cormack <shanemcc@gmail.com>
2017-12-17 11:26:50 +0000

    Day 16 changes - don't assume we always loop back to 1

--- 16/run.php
+++ 16/run.php
@@ -5,10 +5,11 @@
 	$dance = explode(',', $input);
 
 	$line = [];
-
 	for ($i = 0; $i < (isTest() ? 5 : 16); $i++) { $line[] = chr(97 + $i); }
+	$line = implode('', $line);
 
 	function doDance($line, $dance) {
+		$line = str_split($line);
 		foreach ($dance as $step) {
 			if (preg_match('#s([0-9]+)#', $step, $m)) {
 				$len = count($line);
@@ -18,20 +19,27 @@
 				$a = ($m[1] == 'x') ? $m[2] : array_search($m[2], $line);
 				$b = ($m[1] == 'x') ? $m[3] : array_search($m[3], $line);
 
-				$line[$a] ^= $line[$b];
-				$line[$b] ^= $line[$a];
-				$line[$a] ^= $line[$b];
+				$line[$a] ^= $line[$b] ^= $line[$a] ^= $line[$b];
 			}
 		}
+		return implode('', $line);
+	}
+
+	function multiDance($line, $dance, $times) {
+		$known = [];
+		for ($i = 0; $i < $times; $i++) {
+			if ($loopback = array_search($line, $known)) {
+				$loopsize = ($i - 1) - ($loopback - 1);
+				return $known[(($times - $i) % $loopsize) + $loopback];
+			} else {
+				$known[] = $line;
+				$line = doDance($line, $dance);
+			}
+		}
+
 		return $line;
+
 	}
 
-	// Get all possible cycles of the dance
-	$known = [];
-	while (array_search(implode('', $line), $known) === FALSE) {
-		$known[] = implode('', $line);
-		$line = doDance($line, $dance);
-	}
-
-	echo 'Part 1: ', $known[1], "\n";
-	echo 'Part 2: ', $known[1000000000 % count($known)], "\n";
+	echo 'Part 1: ', multiDance($line, $dance, 1), "\n";
+	echo 'Part 2: ', multiDance($line, $dance, 1000000000), "\n";
