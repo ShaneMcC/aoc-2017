72bf94893f84a377ff6da7d235e9ad2ad0b903e0
Shane Mc Cormack <shanemcc@gmail.com>
2017-12-19 11:57:51 +0000

    Smaller window.

--- 19/fluff.php
+++ 19/fluff.php
@@ -4,26 +4,30 @@
 	$__CLI['extrahelp'][] = '      --draw               Draw the entire path as we follow it.';
 	$__CLI['extrahelp'][] = '      --sleep <#>          Sleep time between output when using drawSearch';
 
-	function drawState($map, $pos, $max, $collected, $steps, $redraw = true) {
+	function drawState($map, $pos, $max, $collected, $steps, $redraw = true, $windowSize = ['w' => 80, 'h' => 40]) {
 		global $visited, $__CLIOPTS;
 
 		if (!isset($__CLIOPTS['draw'])) { return; }
 
 		$visited[$pos['y']][$pos['x']] = true;
 
-		if ($redraw) { echo "\033[" . ($max['y'] + 5) . "A"; }
+		if ($redraw) { echo "\033[" . ($windowSize['h'] + 5) . "A"; }
 
-		echo '┍', str_repeat('━', $max['x']), '┑', "\n";
-		for ($y = 0; $y < $max['y']; $y++) {
+		$offset = $pos;
+		$offset['x'] -= floor($windowSize['w'] / 2);
+		$offset['y'] -= floor($windowSize['h'] / 2);
+
+		echo '┍', str_repeat('━', $windowSize['w']), '┑', "\n";
+		for ($y = $offset['y']; $y < $offset['y'] + $windowSize['h']; $y++) {
 			echo '│';
-			for ($x = 0; $x < $max['x']; $x++) {
+			for ($x = $offset['x']; $x < $offset['x'] + $windowSize['w']; $x++) {
 				if (isset($visited[$y][$x])) { echo "\033[0;32m"; }
 				echo isset($map[$y][$x]) ? $map[$y][$x] : ' ';
 				if (isset($visited[$y][$x])) { echo "\033[0m"; }
 			}
 			echo '│', "\n";
 		}
-		echo '┕', str_repeat('━', $max['x']), '┙', "\n";
+		echo '┕', str_repeat('━', $windowSize['w']), '┙', "\n";
 		echo 'Collected: ', implode('', $collected), "\n";
 		echo 'Steps: ', $steps, "\n";
 		echo "\n";
