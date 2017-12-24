de5a45511266e63f129a1f1d7c1fe75b0b6371d4
Shane Mc Cormack <shanemcc@gmail.com>
2017-12-24 19:47:20 +0000

    Rewrite Day 24.

--- 24/run.php
+++ 24/run.php
@@ -23,42 +23,32 @@
 		return $results;
 	}
 
+	function findBridges($connector, $current = []) {
+		global $components;
+
+		$possible = findComponents($connector, $current);
+		$result = empty($current) ? [] : [$current];
+
+		foreach ($possible as $p) {
+			$nextConnector = ($components[$p]['pins'][0] == $connector) ? $components[$p]['pins'][1] : $components[$p]['pins'][0];
+			$result = array_merge($result, findBridges($nextConnector, array_merge($current, [$p])));
+		}
+
+		return $result;
+	}
+	$finishedBridges = findBridges(0);
+
 	function bridgeInfo($bridge) {
 		global $components;
 
 		$score = 0;
 		$b = [];
-		foreach ($bridge['bridge'] as $bits) {
+		foreach ($bridge as $bits) {
 			$score += $components[$bits]['score'];
 			$b[] = implode('/', $components[$bits]['pins']);
 		}
 		return [implode('--', $b), $score];
 	}
-
-	$finishedBridges = [];
-	$possibleBridges = [];
-
-	// Starting Blocks.
-	foreach (findComponents(0) as $b) { $possibleBridges[] = ['bridge' => [$b], 'lastConnector' => 0]; }
-
-	while (!empty($possibleBridges)) {
-		$current = array_shift($possibleBridges);
-
-		// Store valid possible bridge;
-		$finishedBridges[] = $current;
-
-		// Get possible future connectors
-		$lastElement = $components[$current['bridge'][count($current['bridge']) - 1]]['pins'];
-		$nextConnector = ($lastElement[0] == $current['lastConnector']) ? $lastElement[1] : $lastElement[0];
-		$connections = findComponents($nextConnector, $current['bridge']);
-
-		foreach ($connections as $c) {
-			$possibleBridges[] = ['bridge' => array_merge($current['bridge'], [$c]), 'lastConnector' => $nextConnector];
-		}
-
-		debugOut('possibleBridges: ', count($possibleBridges), "\n");
-	}
-
 
 	$part1 = 0;
 	$part2_len = 0;
@@ -66,10 +56,12 @@
 	foreach ($finishedBridges as $bridge) {
 		$bi = bridgeInfo($bridge);
 
-		if (count($bridge['bridge']) > $part2_len) {
+		if (count($bridge) > $part2_len) {
 			$part2 = $bi[1];
-		} else if (count($bridge['bridge']) == $part2_len) {
+			$part2_len = count($bridge);
+		} else if (count($bridge) == $part2_len) {
 			$part2 = max($part2, $bi[1]);
+			$part2_len = count($bridge);
 		}
 
 		$part1 = max($part1, $bi[1]);
