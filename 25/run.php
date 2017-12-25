9e5a3e03bcf7cff5c34904ca8fecb3ede9773b65
Shane Mc Cormack <shanemcc@gmail.com>
2017-12-25 05:35:46 +0000

    Minor tidying

--- 25/run.php
+++ 25/run.php
@@ -18,55 +18,43 @@
 			} else if (preg_match('#Perform a diagnostic checksum after (.*) steps#SADi', $details, $m)) {
 				$checksum = $m[1];
 			} else if (preg_match('#In state (.*):#SADi', $details, $m)) {
-				if ($currentValue != NULL) {
-					$rules[$currentState][$currentValue] = $currentRule;
-					$currentValue = null;
-				}
+				if ($currentValue != NULL) { $rules[$currentState][$currentValue] = $currentRule; }
 
 				$currentState = $m[1];
+				$currentRule = $currentValue = NULL;
 			} else if (preg_match('#^$#SADi', $details, $m)) {
-				$rules[$currentState][$currentValue] = $currentRule;
+				if ($currentValue != NULL) { $rules[$currentState][$currentValue] = $currentRule; }
 
-				$currentState = NULL;
-				$currentValue = NULL;
-				$currentRule = NULL;
-			} else if ($currentState != NULL) {
-				if (preg_match('#\s+If the current value is (.*):#SADi', $details, $m)) {
-					if ($currentValue != NULL) {
-						$rules[$currentState][$currentValue] = $currentRule;
-					}
+				$currentState = $currentValue = $currentRule = NULL;
+			} else if (preg_match('#\s+If the current value is (.*):#SADi', $details, $m)) {
+				if ($currentValue != NULL) { $rules[$currentState][$currentValue] = $currentRule; }
 
-					$currentValue = $m[1];
-					$currentRule = ['write' => NULL, 'move' => NULL, 'continue' => NULL];
-				} else if (preg_match('#\s+- Write the value (.*).#SADi', $details, $m)) {
-					$currentRule['write'] = $m[1];
-				} else if (preg_match('#\s+- Move one slot to the (.*).#SADi', $details, $m)) {
-					$currentRule['move'] = $m[1] == 'right' ? 1 : -1;
-				} else if (preg_match('#\s+- Continue with state (.*).#SADi', $details, $m)) {
-					$currentRule['continue'] = $m[1];
-				}
+				$currentValue = $m[1];
+				$currentRule = ['write' => NULL, 'move' => NULL, 'state' => NULL];
+			} else if (preg_match('#\s+- Write the value (.*).#SADi', $details, $m)) {
+				$currentRule['write'] = $m[1];
+			} else if (preg_match('#\s+- Move one slot to the (.*).#SADi', $details, $m)) {
+				$currentRule['move'] = $m[1] == 'right' ? 1 : -1;
+			} else if (preg_match('#\s+- Continue with state (.*).#SADi', $details, $m)) {
+				$currentRule['state'] = $m[1];
 			}
 		}
-
-		if ($currentState != NULL) {
-			$rules[$currentState][$currentValue] = $currentRule;
-		}
+		if ($currentState != NULL) { $rules[$currentState][$currentValue] = $currentRule; }
 
 		return [$begin, $checksum, $rules];
 	}
 
-	list($begin, $checksum, $rules) = parseRules($input);
+	[$begin, $checksum, $rules] = parseRules($input);
 
 	$position = 0;
-	$tape = [0 => 0];
-
+	$tape = [];
 	$currentState = $begin;
 	for ($i = 0; $i < $checksum; $i++) {
 		$currentValue = isset($tape[$position]) ? $tape[$position] : 0;
 		$rule = $rules[$currentState][$currentValue];
 
 		$tape[$position] = $rule['write'];
-		$currentState = $rule['continue'];
+		$currentState = $rule['state'];
 		$position += $rule['move'];
 	}
 
