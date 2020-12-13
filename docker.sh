22035ba1080dfae6206ed3c86156edaf986c9755
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-13 18:32:02 +0000

    Minor docker changes to bring all years in line.

--- docker.sh
+++ docker.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
+BASEIMAGE=shanemcc/aoc-2017-05
+BASEDOCKERFILE="Dockerfile"
 
-IMAGE=shanemcc/aoc-2017-04
+IMAGE=${BASEIMAGE}
+DOCKERFILE=${BASEDOCKERFILE}
+FORCEBUILD="0";
+SHELL="0";
+
+while true; do
+	case "$1" in
+#		--jit|--hhvm)
+#			IMAGE=${BASEIMAGE}-${1/--/}
+#			DOCKERFILE=${BASEDOCKERFILE}-${1/--/}
+#			;;
+		--build)
+			FORCEBUILD="1";
+			;;
+		--shell)
+			SHELL="1";
+			;;
+		*)
+			break;
+			;;
+	esac
+	shift
+done
 
 docker image inspect $IMAGE >/dev/null 2>&1
-if [ $? -ne 0 ]
-then
-    echo "One time setup: building docker image..."
-    cd docker
-    docker build . -t $IMAGE
-    cd ..
+if [ $? -ne 0 -o ${FORCEBUILD} = "1" ]; then
+	echo "One time setup: building docker image ${IMAGE}..."
+	cd docker
+	docker build . -t $IMAGE --file ${DOCKERFILE}
+	cd ..
+	echo "Image build complete."
 fi
 
-docker run --rm -it -v $(pwd):/code $IMAGE /entrypoint.sh $@
+if [ "${SHELL}" = "1" ]; then
+	docker run --rm -it -v $(pwd):/code $IMAGE bash
+else
+	docker run --rm -it -v $(pwd):/code $IMAGE /entrypoint.sh $@
+fi;
